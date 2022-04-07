779b6dc073c9d787a6397bfee8f9ef05f46ab29d
MuhammadHannan246 <102234883+MuhammadHannan246@users.noreply.github.com>
2022-04-07 15:46:45 +0500

    Using PIR Sensor

---
+++ PIR Sensor.ino
@@ -0,0 +1,26 @@
+// C++ code
+//
+int pir = 2;
+int led = 3;
+void setup()
+{
+  pinMode(pir, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int i = digitalRead(pir);
+  Serial.print(i);
+  if(i==1){
+  digitalWrite(led, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+ 
+    
+  }
+  else{
+  digitalWrite(led, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
+}
